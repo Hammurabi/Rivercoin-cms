57647aa1c5f173bac754e1145353f0a27a2657c0
Ragnarr Ivarssen <Riverssen@gmail.com>
2018-06-25 04:12:49 +0200

    initial commit, setting up

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+class webpage{
+    public function loadCustomMenuContent()
+    {
+    }
+
+    public function loadHTMLContent()
+    {
+    }
+
+    public function loadCSSContent()
+    {
+    }
+
+    public function loadContent()
+    {
+    }
+}
+?>
