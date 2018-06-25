ebcded8428cad2feff2e05eef17dd6531d50507a
Ragnarr Ivarssen <Riverssen@gmail.com>
2018-06-25 04:18:07 +0200

    added plugin support (basic)

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
 <?php
+    class plugin{
+        static $STEP_INITIAL        = 0;
+        static $STEP_HEAD           = 1;
+        static $STEP_BODY_MENU      = 2;
+        static $STEP_BODY_CONTENT   = 3;
+        static $STEP_BODY_FOOTER    = 4;
+
+        public function execute($step)
+        {
+            switch($step)
+            {
+                case $STEP_INITIAL:
+                break;
+                case $STEP_HEAD:
+                break;
+                case $STEP_BODY_MENU:
+                break;
+                case $STEP_BODY_CONTENT:
+                break;
+                case $STEP_BODY_FOOTER:
+                break;
+            }
+        }
+    }
     class webpage{
         public function loadCustomMenuContent()
         {
